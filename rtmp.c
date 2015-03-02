41e46fdc6a7a4487fbfa00f34a9b93b8288b29a7
Akagi201 <akagi201@gmail.com>
2015-03-03 02:06:34 +0800

    comment RTMP_GetTime

--- rtmp.c
+++ rtmp.c
@@ -174,15 +174,19 @@ static int clk_tck;
 
 #endif
 
-uint32_t
-RTMP_GetTime() {
+/*
+ * @brief get runtime in ms
+ */
+uint32_t RTMP_GetTime() {
 #ifdef _DEBUG
   return 0;
 #elif defined(_WIN32)
   return timeGetTime();
 #else
     struct tms t;
-    if (!clk_tck) clk_tck = sysconf(_SC_CLK_TCK);
+    if (!clk_tck) {
+        clk_tck = sysconf(_SC_CLK_TCK); // ticks per second
+    }
     return times(&t) * 1000 / clk_tck;
 #endif
 }
